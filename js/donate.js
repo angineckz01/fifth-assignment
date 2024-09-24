8a1dad0a87bd8f1354ede41c8d42bb291469a191
Your Name <angineckz01@gmail.com>
2024-09-24 09:08:57 +0600

    added page and modify some section

--- js/donate.js
+++ js/donate.js
@@ -8,6 +8,11 @@ document.getElementById('btn-donate-now-noakhali').addEventListener('click', fun
     }
     // get id using function
     const accountBalance = donnateGetTextValueById('acount-balance');
+    // condition for donate
+    if (inputDonateNoakhali > accountBalance || inputDonateNoakhali===''){
+        alert('You have insufficient money to donate for noakhali');
+        return;
+    }
     const donateNoakhali = donnateGetTextValueById('donate-noakhali');
     // noakhali total fund = current fund + amount to be donated
     const donateNoakhaliBalance =donateNoakhali + inputDonateNoakhali;
@@ -15,7 +20,21 @@ document.getElementById('btn-donate-now-noakhali').addEventListener('click', fun
     // available balance = deducted donated amount from accountBalance
     const availableBalance = accountBalance - inputDonateNoakhali;
     document.getElementById('acount-balance').innerText = availableBalance;
+
+    // add to transaction history
+    // donation amount and current ammount
+    const donateNoakhaliTitle = document.getElementById('donate-noakhali-title').innerText;
+    const p = document.createElement('p');
+    p.classList.add('bg-gray-300');
+    p.classList.add('text-black');
+    p.classList.add('text-xl');
+    p.classList.add('text-center');
+    p.innerText = `Donated : ${inputDonateNoakhali} Tk ${donateNoakhaliTitle}. Available balance : ${availableBalance}`;
+    console.log(p);
+    document.getElementById('transaction-container').appendChild(p);
 })
+
+
 
 // doate for feni
 document.getElementById('btn-donate-now-feni').addEventListener('click',function(event){
@@ -27,14 +46,36 @@ document.getElementById('btn-donate-now-feni').addEventListener('click',function
     }
     // get id using function
     const accountBalance = donnateGetTextValueById('acount-balance');
+    // condition for donate
+    if (inputDonateFeni > accountBalance || inputDonateFeni===''){
+        alert('You have insufficient money to donate for feni');
+        return;
+    }
     const donateFeni = donnateGetTextValueById('donate-feni');
     // feni total fund = current + donated
     const donateFeniBalance = donateFeni + inputDonateFeni;
     document.getElementById('donate-feni').innerText = donateFeniBalance;
     // till now current available balance
-    availableBalance = accountBalance - inputDonateFeni;
+    const availableBalance = accountBalance - inputDonateFeni;
     document.getElementById('acount-balance').innerText = availableBalance;
+
+    // add to transaction history
+    // donation amount and current ammount
+    const donateFeniTitle = document.getElementById('donate-feni-title').innerText;
+    const p = document.createElement('p');
+    p.classList.add('bg-gray-200');
+    p.classList.add('text-black');
+    p.classList.add('text-xl');
+    p.classList.add('text-center');
+    p.innerText = `Donated : ${inputDonateFeni} Tk ${donateFeniTitle}. Available balance : ${availableBalance}`;
+    console.log(p);
+    document.getElementById('transaction-container').appendChild(p);
+
 })
+
+
+
+
 
 // donate for quota protest
 document.getElementById('btn-donate-now-quota-protest').addEventListener('click',function(event){
@@ -46,11 +87,38 @@ document.getElementById('btn-donate-now-quota-protest').addEventListener('click'
     }
     // get id using function
     const accountBalance = donnateGetTextValueById('acount-balance');
+    // condition for donate
+    if (inputDonateQoutaProtest > accountBalance || inputDonateQoutaProtest===''){
+        alert('You have insufficient money to donate for qouta protest');
+        return;
+    }
     const donateQuotaProtest = donnateGetTextValueById('fund-quota-protest');
     // quota protest total fund
     const donateProtestBalance = donateQuotaProtest + inputDonateQoutaProtest;
     document.getElementById('fund-quota-protest').innerText = donateProtestBalance;
     // till now current available balance
-    availableBalance = accountBalance - inputDonateQoutaProtest;
+    const availableBalance = accountBalance - inputDonateQoutaProtest;
     document.getElementById('acount-balance').innerText = availableBalance;
+
+    // add to transaction history
+    // donation amount and current ammount
+    const donateProtestTitle = document.getElementById('donate-protest-title').innerText;
+    const p = document.createElement('p');
+    p.classList.add('bg-red-100');
+    p.classList.add('text-black');
+    p.classList.add('text-xl');
+    p.classList.add('text-center');
+    p.innerText = `Donated : ${inputDonateQoutaProtest} Tk ${donateProtestTitle}. Available balance : ${availableBalance}`;
+    console.log(p);
+    document.getElementById('transaction-container').appendChild(p);
 })
+
+
+
+// show section when clicked
+document.getElementById('show-donation-section').addEventListener('click',function(){
+    showSectionById('donation-section');
+});
+document.getElementById('show-transaction-history').addEventListener('click',function(){
+    showSectionById('transaction-section');
+});
--- js/function.js
+++ js/function.js
@@ -9,4 +9,13 @@ function getInputFieldValueById(id){
     const inputNumber = parseFloat(inputValue);
     return inputNumber;
 }
+
+
+function showSectionById(id){
+    // default hidden all section
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+    // get the expected section by calling function parameter
+    document.getElementById(id).classList.remove('hidden')
+}
 // console.log('hello world from function js');
